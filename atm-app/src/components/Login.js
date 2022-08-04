a7eb5af0e5f27a0a98060b844065a6b171f58f24
Edward Sierra <edward.sierrac@gmail.com>
2022-08-03 23:04:19 -0500

    COMMIT 3

---
+++ atm-app/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Login.css'
+
+function Login (props) {
+
+  return (
+    <div className='contenedor-login'>
+      <div className='contendor-titulo' >
+        <h1>Ingresar a No Name</h1>
+      </div>
+      <div className='contenedor-username'>
+      </div>
+      <div className='contenedor-password'>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
