2cc704f07916387844d6fdc47a97afba069fb33e
Edward Sierra <edward.sierrac@gmail.com>
2022-08-08 06:53:20 -0500

    COMMIT 4

--- atm-app/src/components/Login.js
+++ atm-app/src/components/Login.js
@@ -9,9 +9,12 @@ function Login (props) {
         <h1>Ingresar a No Name</h1>
       </div>
       <div className='contenedor-username'>
+        <input className='txt-user' type="text" name="" id="" />
       </div>
       <div className='contenedor-password'>
+        <input className='txt-password' type="password" name="" id="" />
       </div>
+      <input className='btn-login' type="button" value="Ingresar" />
     </div>
   );
 }
